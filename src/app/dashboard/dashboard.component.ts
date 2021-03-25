7cacb2e440e463dbf8048e1a21eaa0b4e8246699
Fuerli-exe <141afuerste@eurogym.info>
2021-03-25 14:14:59 +0100

    Tabelle nach Aufgaben verschoben

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { Todo } from '../shared/todo';
 import { TodoListService } from '../shared/todo-list.service';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,6 +10,14 @@ import { TodoListService } from '../shared/todo-list.service';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  public todoDescription = '';
+  public todoDueDate = formatDate(new Date(), 'yyyy-MM-dd', 'en');
+  public showDone = false;
+  public subject ='';
+  public group ='';
+  public category ='';
+  public displayedColumns = ['actions','todoDueDate','subject','group', 'category', 'todoDescription','deleteTodoById' ]
+
   constructor(public todoListService: TodoListService) {
   }
 
