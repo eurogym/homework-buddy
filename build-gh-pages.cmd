d238b5dec3e175d7d9facccdb14eb4cc0cde28a0
Gerald Landl <glandl@duality.at>
2021-08-16 22:48:30 +0200

    Reverted to Angular 11
    Angular 12 makes problems with firedb permissions

--- build-gh-pages.cmd
+++ build-gh-pages.cmd
@@ -1,2 +1,2 @@
-ng build --prod --base-href "https://eurogym.github.io/homework-buddy/"
+ng build --prod --base-href "https://eurogym.github.io/homework-buddy-pub/"
 npx angular-cli-ghpages --dir=dist/homework-buddy
